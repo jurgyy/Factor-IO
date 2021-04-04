fd948905b0ccc01ff256a48588fbab9e67785f34
Jurgen <jurgen.baas@outlook.com>
2021-04-04 17:31:45 +0200

    Entity class now centers its position.

--- Blueprint/Entity.py
+++ Blueprint/Entity.py
@@ -1,5 +1,5 @@
 import json
-from typing import List
+from typing import List, Tuple
 from math import cos, sin
 import numpy as np
 
@@ -123,9 +123,10 @@ class Entity:
         self.color: Color = None if color is None else Color(**color)
         self.station: str = station
 
-        (x, y), (w, h) = Entity.get_entity_size_dict()[self.name]
-        self.half_width = w
-        self.half_height = h
+        (l, d), (r, u) = Entity.get_entity_size_dict()[self.name]
+        self.half_width: Tuple[float, float] = (l, r)
+        self.half_height: Tuple[float, float] = (d, u)
+        self._correct_position()
 
     def __repr__(self):
         return f"[Entity {self.name}@{self.position} D={self.direction}]"
@@ -134,15 +135,15 @@ class Entity:
     def bounding_box(self) -> np.ndarray:
         if self.direction % 2 == 0:
             # 90 degree turns
-            x_dist, y_dist = self.half_width, self.half_height
+            (l, r), (d, u) = self.half_width, self.half_height
 
             if self.direction % 4 != 0:
-                x_dist, y_dist = y_dist, x_dist
+                (l, r), (d, u) = (d, u), (l, r)
 
-            min_x = round(self.position.x - x_dist)
-            max_x = round(self.position.x + x_dist)
-            min_y = round(self.position.y - y_dist)
-            max_y = round(self.position.y + y_dist)
+            min_x = int(Entity._round_corrected(self.position.x + l))
+            max_x = int(Entity._round_corrected(self.position.x + r))
+            min_y = int(Entity._round_corrected(self.position.y + d))
+            max_y = int(Entity._round_corrected(self.position.y + u))
 
             corners = np.array([[min_x, min_x, max_x, max_x], [min_y, max_y, min_y, max_y]])
 
@@ -155,8 +156,44 @@ class Entity:
             # theta = np.deg2rad(360 - degrees)
             # return utils.math.rotate(np.array([self.position.x, self.position.y]), corners, theta).T
 
+    @staticmethod
+    def _round_to_corrected_halve(value: float):
+        if value <= -0.00390625:
+            # value - 1/256
+            # see https://forums.factorio.com/viewtopic.php?f=23&t=97568&p=542718
+            return np.floor(value + 0.00390625) + 0.5
+        return np.floor(value) + 0.5
 
+    @staticmethod
+    def _round_corrected(value: float):
+        #if value > 0:
+        #    if value >= 0.5:
+        #        return np.ceil(value)
+        #    return np.floor(value)
+        return int(np.floor(value + 0.5))#np.round(value)
 
+    def _correct_position(self):
+        """"
+            1x1 = (0.5, 0.5)
+            1x2 = (1,   0.5)
+            2x2 = (1,     1)
+            3x3 = (1.5, 1.5)
+            4x4 = (2,     2)
+            9x9 = (4.5, 4.5)
+        """
+        width = round((self.half_width[1] - self.half_width[0]) * 2) / 2
+        height = round((self.half_height[1] - self.half_height[0]) * 2) / 2
 
+        if self.direction == 2 or self.direction == 6:
+            # direction = rotated 90 or 270 degrees
+            width, height = height, width
 
+        if width % 2 == 0:
+            self.position.x = Entity._round_corrected(self.position.x)
+        else:
+            self.position.x = Entity._round_to_corrected_halve(self.position.x)
 
+        if height % 2 == 0:
+            self.position.y = Entity._round_corrected(self.position.y)
+        else:
+            self.position.y = Entity._round_to_corrected_halve(self.position.y)
--- Blueprint/Tests/test_Entity.py
+++ Blueprint/Tests/test_Entity.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 import numpy as np
 
 from Blueprint.Entity import EntityDict, Entity
-from Blueprint.Position import PositionDict
+from Blueprint.Position import PositionDict, Position
 
 
 def get_entity_dict(entity_name: str, x: float, y: float, direction: int = 0) -> EntityDict:
@@ -18,28 +18,68 @@ class TestEntity(TestCase):
     def test_bounding_box_3x3(self):
         e = get_entity("assembling-machine-1", 0.5, 0.5)
         expected = np.array(([-1, -1], [-1, 2], [2, -1], [2, 2]))
-        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected: {expected}\nActual: {e.bounding_box}")
+        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected:\n{expected}\nActual:\n{e.bounding_box}")
 
         e = get_entity("assembling-machine-1", 1.5, 1.5)
         expected = np.array(([0, 0], [0, 3], [3, 0], [3, 3]))
-        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected: {expected}\nActual: {e.bounding_box}")
+        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected:\n{expected}\nActual:\n{e.bounding_box}")
 
         e = get_entity("assembling-machine-1", -0.5, -0.5)
         expected = np.array(([-2, -2], [-2, 1], [1, -2], [1, 1]))
-        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected: {expected}\nActual: {e.bounding_box}")
+        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected:\n{expected}\nActual:\n{e.bounding_box}")
 
         e = get_entity("assembling-machine-1", -1.5, -1.5)
         expected = np.array(([-3, -3], [-3, 0], [0, -3], [0, 0]))
-        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected: {expected}\nActual: {e.bounding_box}")
+        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected:\n{expected}\nActual:\n{e.bounding_box}")
 
     def test_bounding_box_1x2(self):
         e = get_entity("splitter", 1, 0.5)
         expected = np.array(([0, 0], [0, 1], [2, 0], [2, 1]))
-        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected: {expected}\nActual: {e.bounding_box}")
+        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected:\n{expected}\nActual:\n{e.bounding_box}")
+
+        e = get_entity("splitter", 2, 4.5)
+        expected = np.array(([1, 4], [1, 5], [3, 4], [3, 5]))
+        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected:\n{expected}\nActual:\n{e.bounding_box}")
 
         e = get_entity("splitter", 0.5, 1, direction=2)
         expected = np.array(([0, 0], [0, 2], [1, 0], [1, 2]))
-        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected: {expected}\nActual: {e.bounding_box}")
+        self.assertTrue(np.array_equal(e.bounding_box, expected), f"Expected:\n{expected}\nActual:\n{e.bounding_box}")
+
+    def test_correct_position(self):
+        self.assertEqual(Position(1.5, 1.5), get_entity("assembling-machine-1", 1.5, 1.5).position)
+
+        self.assertEqual(Position(1, 0.5), get_entity("splitter", 1, 0.5).position)
+        self.assertEqual(Position(0, 0.5), get_entity("splitter", -0.5, 0).position)
+
+        self.assertEqual(Position(0.5, 0.5), get_entity("transport-belt", 0.5, 0.5).position)
+
+    def test_round_corrected(self):
+        self.assertEqual(0, Entity._round_corrected(-0.5))
+        self.assertEqual(1, Entity._round_corrected(0.5))
+
+    def test_round_to_corrected_halve(self):
+        self.assertEqual(0.5, Entity._round_to_corrected_halve(0))
+        self.assertEqual(0.5, Entity._round_to_corrected_halve(0.2))
+        self.assertEqual(0.5, Entity._round_to_corrected_halve(0.4))
+        self.assertEqual(0.5, Entity._round_to_corrected_halve(0.6))
+        self.assertEqual(0.5, Entity._round_to_corrected_halve(0.8))
+        self.assertEqual(0.5, Entity._round_to_corrected_halve(0.99))
+        self.assertEqual(1.5, Entity._round_to_corrected_halve(1))
+        self.assertEqual(1.5, Entity._round_to_corrected_halve(1.1))
+        self.assertEqual(1.5, Entity._round_to_corrected_halve(1.5))
+        self.assertEqual(1.5, Entity._round_to_corrected_halve(1.99))
+        self.assertEqual(2.5, Entity._round_to_corrected_halve(2))
+
+        self.assertEqual(-0.5, Entity._round_to_corrected_halve(-0.2))
+        self.assertEqual(-0.5, Entity._round_to_corrected_halve(-0.4))
+        self.assertEqual(-0.5, Entity._round_to_corrected_halve(-0.6))
+        self.assertEqual(-0.5, Entity._round_to_corrected_halve(-0.8))
+        self.assertEqual(-0.5, Entity._round_to_corrected_halve(-0.99))
+        self.assertEqual(-0.5, Entity._round_to_corrected_halve(-1))
+        self.assertEqual(-1.5, Entity._round_to_corrected_halve(-1.5))
+        self.assertEqual(-1.5, Entity._round_to_corrected_halve(-1.99))
+        self.assertEqual(-1.5, Entity._round_to_corrected_halve(-2))
+        self.assertEqual(-2.5, Entity._round_to_corrected_halve(-2.1))
 
 
 
