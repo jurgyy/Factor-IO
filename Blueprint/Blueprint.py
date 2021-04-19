b940f589f053191bbeb1c301e7c04dc3c46046e4
Jurgen <jurgen.baas@outlook.com>
2021-04-19 16:13:50 +0200

    Using PEP 589's alternative syntax for Typed dicts such that json fields with hyphens can be incorporated in the BlueprintDict. Added missing fields "description", "snap-to-grid" and "absolute-snapping".

--- Blueprint/Blueprint.py
+++ Blueprint/Blueprint.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import numpy as np
-
 from Blueprint.FactorioBlueprintObject import FactorioBlueprintObject
 from cachedProperty import cached_property
 from typing import List, Tuple, Type
@@ -11,24 +9,32 @@ from typing_extensions import TypedDict
 from Blueprint.Color import Color, ColorDict
 from Blueprint.Entity import Entity, EntityDict, CurvedRailEntity
 from Blueprint.Icon import Icon, IconDict
-from Blueprint.Position import Position
+from Blueprint.Position import Position, PositionDict
 from Blueprint.Schedule import Schedule, ScheduleDict
 from Blueprint.Tile import Tile, TileDict
 
 
-class BlueprintDict(TypedDict):
-    entities: List[EntityDict]
-    item: str
-    version: int
-    label: str
-    icons: List[IconDict]
-    label_color: ColorDict
-    tiles: List[TileDict]
-    schedules: List[ScheduleDict]
+BlueprintDict = TypedDict("BlueprintDict", {
+    "entities": List[EntityDict],
+    "item": str,
+    "version": int,
+    "label": str,
+    "icons": List[IconDict],
+    "label_color": ColorDict,
+    "tiles": List[TileDict],
+    "schedules": List[ScheduleDict],
+    "description": str,
+    "snap-to-grid": PositionDict,
+    "absolute-snapping": bool,
+})
 
 
 class Blueprint(FactorioBlueprintObject):
     dict_type: Type[TypedDict] = BlueprintDict
+    field_translation_map = {
+        "snap_to_grid": "snap-to-grid",
+        "absolute_snapping": "absolute-snapping",
+    }
 
     def __init__(self,
                  entities: List[EntityDict] = (),
@@ -39,16 +45,28 @@ class Blueprint(FactorioBlueprintObject):
                  label_color: ColorDict = None,
                  tiles: List[TileDict] = (),
                  schedules: List[ScheduleDict] = (),
+                 description: str = None,
                  *args, **kwargs
                  ):
+        self.entities: List[Entity] = [Blueprint._create_entity(e) for e in entities]
         self.item: str = item
         self.label: str = label
-        self.entities: List[Entity] = [Blueprint._create_entity(e) for e in entities]
         self.tiles: List[Tile] = [Tile(**t) for t in tiles]
         self.icons: List[Icon] = [Icon(**i) for i in icons]
         self.schedules: List[Schedule] = [Schedule(**s) for s in schedules]
         self.version: int = version
         self.label_color: Color = None if label_color is None else Color(**label_color)
+        self.description: str = description
+
+        self.snap_to_grid: Position
+        if "snap-to-grid" in kwargs:
+            self.snap_to_grid = Position(**kwargs["snap-to-grid"])
+            del kwargs["snap-to-grid"]
+
+        self.absolute_snapping: bool
+        if "absolute-snapping" in kwargs:
+            self.absolute_snapping = kwargs["absolute-snapping"]
+            del kwargs["absolute-snapping"]
 
     def __repr__(self):
         x, y = self.get_dimensions()
