a380ebfc652c2f3479d6c0d0cf7135756fadda1c
Jurgen <jurgen.baas@outlook.com>
2021-04-19 16:19:31 +0200

    Removed unnecessary imports and commented code.

--- Blueprint/BookItem.py
+++ Blueprint/BookItem.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Type
+
 from typing_extensions import TypedDict
 
 from Blueprint.BaseBlueprintItem import BaseBlueprintItem, BaseBlueprintItemDict
@@ -8,8 +9,8 @@ from Blueprint.DeconstructionPlanner import DeconstructionPlanner
 from Blueprint.UpgradePlanner import UpgradePlanner
 
 if TYPE_CHECKING:
-    from Blueprint.Blueprint import Blueprint, BlueprintDict
-    from Blueprint.BlueprintBook import BlueprintBook, BlueprintBookDict
+    from Blueprint.Blueprint import BlueprintDict
+    from Blueprint.BlueprintBook import BlueprintBookDict
 
 
 class BookItemDict(BaseBlueprintItemDict):
@@ -27,7 +28,4 @@ class BookItem(BaseBlueprintItem):
                  upgrade_planner: UpgradePlanner = None
                  ):
         super().__init__(blueprint, blueprint_book)
-        # self.blueprint: Blueprint = blueprint if blueprint is None else Blueprint(**blueprint)
-        # self.blueprint_book: BlueprintBook = blueprint_book if blueprint_book is None else BlueprintBook(**blueprint_book)
-        # self.deconstruction_planner: DeconstructionPlanner = deconstruction_planner
         self.index: int = index
