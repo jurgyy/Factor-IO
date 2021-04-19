5456292ea4c42be3fd84cc000ecc0a431ecca33b
Jurgen <jurgen.baas@outlook.com>
2021-04-19 16:11:54 +0200

    Added field_translation_map dict that classes can use to add fields where the json key doesn't match the class' dict key.
    Optimized to_typed_dict method where the dict keys get checked if they are in the TypedDict before converting all fields recursively instead of afterwards.

--- Blueprint/FactorioBlueprintObject.py
+++ Blueprint/FactorioBlueprintObject.py
@@ -6,10 +6,11 @@ from typing_extensions import TypedDict
 
 class FactorioBlueprintObject(abc.ABC):
     @property
-    @classmethod
     @abc.abstractmethod
-    def dict_type(cls) -> Type[TypedDict]:
-        raise NotImplementedError()
+    def dict_type(self) -> Type[TypedDict]:
+        return TypedDict
+
+    field_translation_map = {}
 
     def to_typed_dict(self) -> TypedDict:
         def to_dict(obj, first=False):
@@ -28,15 +29,22 @@ class FactorioBlueprintObject(abc.ABC):
             elif not first and isinstance(obj, FactorioBlueprintObject):
                 return to_dict(obj.to_typed_dict())
             elif hasattr(obj, '__dict__'):
-                return to_dict(vars(obj))
+                d = vars(obj)
+                if isinstance(obj, FactorioBlueprintObject):
+                    for key in list(d.keys()):
+                        if key not in self.dict_type.__required_keys__ \
+                                and key not in self.dict_type.__optional_keys__ \
+                                and key not in self.field_translation_map:
+                            del d[key]
+                    for key, val in obj.field_translation_map.items():
+                        if key in d:
+                            d[val] = d[key]
+                            del d[key]
+
+                return to_dict(d)
             elif hasattr(obj, '__slots__'):
                 return to_dict(dict((name, getattr(obj, name)) for name in getattr(obj, '__slots__')))
             return obj
 
         d = to_dict(self, first=True)
-        for k in list(d.keys()):
-            if k not in type(self).dict_type.__required_keys__ \
-                    and k not in type(self).dict_type.__optional_keys__:
-                del d[k]
-
-        return type(self).dict_type(d)
+        return self.dict_type(d)
